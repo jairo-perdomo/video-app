acb72a6d65da3c43428be084678d4f8fa05bbbcb
jairoperdomo <jairoperdomo09@gmail.com>
2021-10-31 21:50:19 -0600

    Subiendo proyecto

---
+++ PhotoApp/PhotoApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PhotoApp.Views;
+namespace PhotoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PhotosView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ PhotoApp/PhotoApp/Views/PhotosView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Plugin.Media;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhotoApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhotosView : ContentPage
+    {
+        public PhotosView()
+        {
+            InitializeComponent();
+            btnfoto.Clicked += Btnfoto_Clicked;
+        }
+
+        private async void Btnfoto_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    await DisplayAlert("No hay camara", "No se deteca la camara.", "Ok");
+                    return;
+                }
+
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                
+                {
+                    // Variable para guardar la foto en el album público
+                    SaveToAlbum = true
+                });
+
+                if (file == null)
+                    return;
+
+                this.imgcamara.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+
+                await DisplayAlert("Foto realizada", "Localización: " + file.AlbumPath, "Ok");
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Permiso denegado", "Da permisos de cámara al dispositivo.\nError: " + ex.Message, "Ok");
+            }
+        }
+    }
+}
